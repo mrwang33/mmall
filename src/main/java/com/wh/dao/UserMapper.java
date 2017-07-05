f2bfcb25b83821402c0104cbf8b8934ae486d9fe
wh <wang1386528@gmail.com>
2017-07-05 17:34:18 +0800

    mybatis plugins

---
+++ src/main/java/com/wh/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wh.dao;
+
+import com.wh.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
